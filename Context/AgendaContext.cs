85bf2ce16a4120a29fb4be80cdb26f2cf389dbc5
Davi Martins <davi_martins2009outlook.com>
2024-01-30 17:54:14 -0300

    first commit

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Api_com_EF.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_com_EF.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
