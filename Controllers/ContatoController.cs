79dc06ba88cc3cfef0617defcac62e49bc901d07
Davi Martins <davi_martins2009outlook.com>
2024-01-31 16:02:54 -0300

    Criando Contreoller e Endpoints

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Api_com_EF.Entities;
+using Api_com_EF.Context;
+
+namespace Api_com_EF.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+    }
+}
--- obj/Debug/net8.0/Api_com_EF.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api_com_EF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_com_EF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bf2ce16a4120a29fb4be80cdb26f2cf389dbc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_com_EF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_com_EF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
